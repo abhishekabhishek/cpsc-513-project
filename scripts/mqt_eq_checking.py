9655f8c5ed4c5db7eb9ad43be86cf8bbaadf058a
abhishekabhishek <abhishek@myumanitoba.ca>
2024-12-18 19:09:07 -0800

    EOD office sync: adding mqt circuits

--- scripts/mqt_eq_checking.py
+++ scripts/mqt_eq_checking.py
@@ -1,9 +1,10 @@
-"""Standalone script to perform equivalence checking on qasm benchmark circuits in
-feynman/benchmarks/qasm/ and save the results in a csv file.
+"""Standalone script to perform equivalence checking on classes of benchmark circuits in mqtbench
+and save the results in a csv file.
 """
 
 import logging
 import multiprocessing
+import math
 import random
 
 from multiprocessing import Process
@@ -11,11 +12,12 @@ from multiprocessing import Process
 import pandas as pd
 
 from mqt import qcec
+from mqt.bench import get_benchmark
 from mqt.qcec import Configuration
 from mqt.qcec.configuration import augment_config_from_kwargs
 
 from keys import IBMQ_API
-from qiskit import QuantumCircuit, transpile
+from qiskit import transpile
 
 import qiskit_ibm_runtime
 from qiskit_ibm_runtime import QiskitRuntimeService
@@ -29,23 +31,27 @@ random.seed(0)
 logger = logging.getLogger(__name__)
 
 
-def verify_benchmark(benchmark_name: str, ibm_backend: qiskit_ibm_runtime.ibm_backend.IBMBackend,
+def verify_benchmark(benchmark_name: str, n_qubits: int,
+                     ibm_backend: qiskit_ibm_runtime.ibm_backend.IBMBackend,
                      config: dict, result_dict: dict):
     """Construct the circuits and run verification using qcec.
 
     Args:
         benchmark_name (str): Name of the benchmark circuit.
+        n_qubits (int): No. of qubits to use for the circuit size.
         ibm_backend (qiskit_ibm_runtime.ibm_backend.IBMBackend): Fake or actual IBM backend to
             transpile the circuit to.
         config (dict): Configuration dictionary for qcec.verify().
         result_dict (dict): Result dictionary to save the results of equivalence checking.
     """
-    print(f'Running verification on {benchmark_name}!')
-    # load the qasm file
-    qasm_path = "../feynman/benchmarks/qasm/" + benchmark_name + ".qasm"
-    circ = QuantumCircuit.from_qasm_file(qasm_path)
+    print(f'Running verification on {benchmark_name} with {n_qubits} qubits.')
 
-    # IMPORTANT: add measurements to the end of the circuit
+    # load the benchmark from mqtbench
+    circ = get_benchmark(benchmark_name, level='indep', circuit_size=n_qubits)
+
+    # IMPORTANT: for mqtbench circuits, in order for the verification to work propoerly, we need to
+    # first remove any and then add measurements to all qubits at the end of the circuit
+    circ.remove_final_measurements()
     circ.measure_all()
 
     # transpile to a HW backend
@@ -55,28 +61,34 @@ def verify_benchmark(benchmark_name: str, ibm_backend: qiskit_ibm_runtime.ibm_ba
     result = qcec.verify(circ, circ_hw, config)
 
     # return the times and the results
-    log_string = f'name: {benchmark_name}, preprocessing: {result.preprocessing_time}' +\
+    log_string = f'name: {benchmark_name}, n_qubits: {n_qubits}, ' +\
+        f'preprocessing: {result.preprocessing_time}' +\
         f', check: {result.check_time}, equivalence: {result.equivalence}'
 
     print(log_string)
 
     # add the results to the result_dict to return from the subprocess
-    result_dict['name'] = benchmark_name
+    result_dict['n_qubits'] = n_qubits
     result_dict['check_time'] = result.check_time
     result_dict['result'] = result.equivalence
 
 
 if __name__ == '__main__':
+    # benchmark name
+    benchmark_name = 'qft'
+
+    # setup the range for the n_qubits to explore
+    min_qubits, max_qubits = 2, 40
+    qubit_step_size = 2
+
+    # string to identify this experiment
+    id_string = f'{benchmark_name}_min_{min_qubits}_max_{max_qubits}_step_{qubit_step_size}'
+
     # setup basic logging to a file
-    logging.basicConfig(filename='logs/feynman_eq_checking_results.log', level=logging.INFO)
-
-    # load the csv containing benchmark circuit properties
-    circ_info_df = pd.read_csv('feynman_benchmark_properties.csv')
-    circ_info_df = circ_info_df.sort_values(by='n_gates_original')
-
-    # extract the benchmark names from the df
-    benchmark_names = circ_info_df.name
-    benchmark_names = benchmark_names.to_list()
+    logging.basicConfig(
+        filename=f'logs/mqtbench_{id_string}_eq_checking_results.log',
+        level=logging.INFO
+    )
 
     # setup the configuration for mqt qcec
     config = Configuration()
@@ -112,10 +124,11 @@ if __name__ == '__main__':
     ibm_backend = service.backend("ibm_sherbrooke")
     ext_timeout = 3600.
 
-    benchmark_results_dict = {'name': [], 'check_time': [], 'result': []}
+    benchmark_results_dict = {'n_qubits': [], 'check_time': [], 'result': []}
+    n_steps = math.floor((max_qubits-min_qubits)/qubit_step_size)
 
-    for i, benchmark_name in enumerate(benchmark_names):
-        print(f"{i}/{len(benchmark_names)}")
+    for i, n_qubits in enumerate(range(min_qubits, max_qubits+1, qubit_step_size)):
+        print(f'{i}/{n_steps}')
 
         # initialize the dictionary to save the result of the equivalence checking
         manager = multiprocessing.Manager()
@@ -123,7 +136,8 @@ if __name__ == '__main__':
 
         prc = Process(
             target=verify_benchmark,
-            kwargs={'benchmark_name': benchmark_name, 'ibm_backend': ibm_backend, 'config': config,
+            kwargs={'benchmark_name': benchmark_name, 'n_qubits': n_qubits,
+                    'ibm_backend': ibm_backend, 'config': config,
                     'result_dict': result_dict},
             name=f'process_verify_{benchmark_name}'
         )
@@ -137,9 +151,9 @@ if __name__ == '__main__':
                 benchmark_results_dict[key].append(result_dict[key])
 
         if prc.exitcode is None:
-            print(f'verification for {benchmark_name} unsucessful in {ext_timeout} seconds.')
+            print(f'verification for {benchmark_name}_{n_qubits} unsucessful in {ext_timeout} sec.')
 
     # save the result of the experiments as csv to disk
     print(result_dict, benchmark_results_dict)
     result_df = pd.DataFrame.from_dict(benchmark_results_dict)
-    result_df.to_csv("results/feynman_eq_checking_results.csv")
+    result_df.to_csv(f'results/mqtbench_{id_string}_eq_checking_results.csv')
